47a85f43de9d9b2143f9ca2b15264766c8ca1b29
Yurapon Tinlapat <you@example.com>
2024-10-04 16:32:05 +0700

    complete ex1-ex6

--- ex-6.js
+++ ex-6.js
@@ -1,6 +1,6 @@
 //Start coding here
 const fruits = ["apple", "banana", "orange"];
 
-console.log("First Fruit: " + fruits[1]);
-console.log("Second Fruit: " + fruit[2]);
-console.log("Third Fruit: " + fruits[3]);
+console.log("First Fruit: " + fruits[0]); //chage index to 0
+console.log("Second Fruit: " + fruits[1]); //chage index to 1,add "s" fruit"s"
+console.log("Third Fruit: " + fruits[2]); //chage index to 2
